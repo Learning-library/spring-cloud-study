9bc1c1c683be5dc8d5b0908966155233ab7a1c67
MR.Chang <mrchang@jetbrains.org.cn>
2018-03-02 13:09:54 +0800

    使用Hystrix Dashboard来展示Hystrix用于熔断的各项度量指标。通过Hystrix Dashboard，我们可以方便的查看服务实例的综合情况，比如：服务调用次数、服务调用延迟等。但是仅通过Hystrix Dashboard我们只能实现对服务当个实例的数据展现，在生产环境我们的服务是肯定需要做高可用的，那么对于多实例的情况，我们就需要将这些度量指标数据进行聚合。下面，在本篇中，我们就来介绍一下另外一个工具：Turbine。 Turbine 前面还可以加mq实现异步队列监视，spring-cloud-eureka-consumer-ribbon-hystrix 调用 spring-cloud-eureka-client 后，spring-cloud-eureka-consumer-ribbon-hystrix 数据推给mq，mq推给Turbine，Turbine推给Hystrix Dashboard，Hystrix Dashboard显示数据状态给用户查看

---
+++ spring-cloud-turbine/src/main/java/cn/org/jetbrains/SpringCloudTurbineApplication.java
@@ -0,0 +1,31 @@
+package cn.org.jetbrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * 使用Hystrix Dashboard来展示Hystrix用于熔断的各项度量指标。
+ * 通过Hystrix Dashboard，我们可以方便的查看服务实例的综合情况，
+ * 比如：服务调用次数、服务调用延迟等。
+ * 但是仅通过Hystrix Dashboard我们只能实现对服务当个实例的数据展现，
+ * 在生产环境我们的服务是肯定需要做高可用的，
+ * 那么对于多实例的情况，我们就需要将这些度量指标数据进行聚合。
+ * 下面，在本篇中，我们就来介绍一下另外一个工具：Turbine。
+ */
+
+@SpringBootApplication
+@EnableTurbine
+@EnableEurekaClient
+@EnableAutoConfiguration
+@Configuration
+public class SpringCloudTurbineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudTurbineApplication.class, args);
+    }
+}
