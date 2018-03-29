1106761d119df34a773c75879153ac015c335330
MR.Chang <mrchang@jetbrains.org.cn>
2018-03-29 15:10:09 +0800

    修改配置

--- spring-cloud-eureka-consumer-ribbon-hystrix-dashboard/src/main/java/cn/org/jetbrains/SpringCloudEurekaConsumerRibbonHystrixDashboardApplication.java
+++ spring-cloud-eureka-consumer-ribbon-hystrix-dashboard/src/main/java/cn/org/jetbrains/SpringCloudEurekaConsumerRibbonHystrixDashboardApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableCircuitBreaker
 @EnableHystrix
+@EnableEurekaClient
 public class SpringCloudEurekaConsumerRibbonHystrixDashboardApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudEurekaConsumerRibbonHystrixDashboardApplication.class, args);
